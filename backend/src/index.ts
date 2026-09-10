0841c60b442d9e0edce74f89c9ffdb405f029617
Ageyan <ageenko01114444@gmail.com>
2026-09-10 22:06:49 +0300

    Update domain name CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cors({
     origin: [
         'http://localhost:5173', 
-        'https://andreii-sharing-platform.vercel.app' 
+        'https://aheienko-sharing-platform.vercel.app' 
     ],
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
